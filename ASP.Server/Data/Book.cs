55f22a222f24ffb1ea0891c7bf687597b4a5c291
omar <vrtlvrtl89@gmail.com>
2022-02-15 11:36:22 +0100

    data

--- ASP.Server/Data/Book.cs
+++ ASP.Server/Data/Book.cs
@@ -11,6 +11,13 @@ namespace ASP.Server.Model
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Autheur { get; set; }
+        public string Price { get; set; }
+        public string Content { get; set; }
+        public ICollection<Genre> Genres { get; set; }
+
+
 
         // Mettez ici les propriété de votre livre: Nom, Autheur, Prix, Contenu et Genres associés
         // N'oublier pas qu'un livre peut avoir plusieur genres
