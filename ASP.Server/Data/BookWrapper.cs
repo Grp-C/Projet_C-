5013c64983f680da78414ac1d6e34a10a939ce53
Asmae CHAH <asmaechah30@gmail.com>
2022-02-16 11:36:26 +0100

    API - create bookWrapper to exclude Content from result

---
+++ ASP.Server/Data/BookWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ASP.Server.Model
+{
+    public class BookWrapper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+
+        public ICollection<Genre> Genres { get; set; }
+    }
+}
