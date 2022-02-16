3fb85d833a252d4c489bf67c8f4e2f4fbc6a2d6e
Nouhaila98 <aanaya.nouhaila.emsi@gmail.com>
2022-02-16 14:23:02 +0100

    fixing genre controller & creating the view

--- ASP.Server/Controllers/GenreController.cs
+++ ASP.Server/Controllers/GenreController.cs
@@ -18,6 +18,14 @@ namespace ASP.Server.Controllers
             this.libraryDbContext = libraryDbContext;
         }
 
+
         // A vous de faire comme BookController.List mais pour les genres !
+        public ActionResult<IEnumerable<Genre>> List()
+        {
+            // récupérer les livres dans la base de donées pour qu'elle puisse être affiché
+            
+            List<Genre> ListGenres = libraryDbContext.Genre.ToList(); ;
+            return View(ListGenres);
+        }
     }
 }
