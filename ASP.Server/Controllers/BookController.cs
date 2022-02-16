62a5e7a1accc36d43d0f7fae4fbf8d96556d804f
omar <vrtlvrtl89@gmail.com>
2022-02-16 15:24:16 +0100

    add book

--- ASP.Server/Controllers/BookController.cs
+++ ASP.Server/Controllers/BookController.cs
@@ -36,14 +36,14 @@ namespace ASP.Server.Controllers
             if (ModelState.IsValid)
             {
                 // Il faut intéroger la base pour récupérer l'ensemble des objets genre qui correspond aux id dans CreateBookModel.Genres
-                List<Genre> genres = null;
+                List<Genre> genres = libraryDbContext.Genre.Where(genre=>book.Genres.Contains(genre.Id)).ToList();
                 // Completer la création du livre avec toute les information nécéssaire que vous aurez ajoutez, et metter la liste des gener récupéré de la base aussi
-                libraryDbContext.Add(new Book() {  });
+                libraryDbContext.Add(new Book() { Author=book.Author,Name=book.Name,Id= libraryDbContext.Books.Include(book => book.Genres).ToList().Last().Id+1,Price=book.Price,Content=book.Content,Genres=genres});
                 libraryDbContext.SaveChanges();
             }
 
             // Il faut interoger la base pour récupérer tous les genres, pour que l'utilisateur puisse les slécétionné
-            return View(new BookModel() { AllGenres = null } );
+            return View(new BookModel() { AllGenres = libraryDbContext.Genre.ToList() } );
         }
     }
 }
