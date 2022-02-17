c531fae6c367fabb709aec0f044911ab73132944
Asmae CHAH <asmaechah30@gmail.com>
2022-02-17 11:34:50 +0100

    Application windows creation de Models

---
+++ WPF.Reader/Model/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Reader.Model
+{
+    internal class Genre
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Picture { get; set; }
+
+        public List<int> Books { get; set; }
+    }
+}
