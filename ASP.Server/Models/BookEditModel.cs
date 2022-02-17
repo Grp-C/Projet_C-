e53f8031d0347e8a27438fd78628a5efebe29473
Asmae CHAH <asmaechah30@gmail.com>
2022-02-17 10:32:15 +0100

    Modification book

---
+++ ASP.Server/Models/BookEditModel.cs
@@ -0,0 +1,19 @@
+﻿using ASP.Server.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.Server.Models
+{
+    public class BookEditModel
+    {
+       [Required]
+       public BookModel Book { get; set; }
+
+        // Liste des genres séléctionné par l'utilisateur
+        public List<Genre> Genres { get; set; }
+
+
+        
+    }
+}
