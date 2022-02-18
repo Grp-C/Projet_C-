84fc9ffbbd8243fd4c6c5fb548bb406bfe0d502c
zakidridi <49376062+zakidridi@users.noreply.github.com>
2022-02-18 19:33:48 +0100

    with api Read

--- WPF.Reader/ViewModel/ReadBook.cs
+++ WPF.Reader/ViewModel/ReadBook.cs
@@ -1,20 +1,26 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System.ComponentModel;
 using WPF.Reader.API;
-using WPF.Reader.Model;
+using WPF.Reader.Service;
 
 namespace WPF.Reader.ViewModel
 {
     class ReadBook : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        public BookWrapper CurrentBook { get; init; }
+        public Book CurrentBook { get; init; }
         public ReadBook(BookWrapper book)
         {
-            CurrentBook = book;
+            CurrentBook = Ioc.Default.GetRequiredService<LibraryService>().getBooksbyid(book.Id); ;
         }
         // A vous de jouer maintenant
     }
 
     /* Cette classe sert juste a afficher des donnée de test dans le designer */
-   
+    class InDesignReadBook : ReadBook
+    {
+        public InDesignReadBook() : base(new BookWrapper())
+        {
+        }
+    }
 }
